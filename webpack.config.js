632e56204f2716e2d0408075d287e81a69440108
Bartosz Legięć <bartosz@legiec.eu>
2019-05-05 22:23:28 +0200

    Move webpack to dev dependecies

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,7 @@ const exp = {
 if (process.env.NODE_ENV !== "production") {
   exp.externals = {
     react: "commonjs react", // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+    "prop-types": "commonjs prop-types", // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
   };
 } else {
   exp.devtool = "#source-map";
