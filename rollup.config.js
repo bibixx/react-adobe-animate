fe4f1121c1c080281ab7cb48b3c7bc9ec3a33e95
bibixx <bartosz@legiec.io>
2020-12-31 15:33:18 +0100

    build(deps): update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
-import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
 
 import pkg from './package.json';
 
@@ -32,15 +32,6 @@ export default {
     }),
     commonjs({
       include: ['node_modules/**'],
-      namedExports: {
-        'node_modules/react/react.js': [
-          'Children',
-          'Component',
-          'PropTypes',
-          'createElement',
-        ],
-        'node_modules/react-dom/index.js': ['render'],
-      },
     }),
   ],
 };
